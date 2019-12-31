d46dee760414c60e45e9c9999fd704dca51a23c6
T. Sivula <tsivula@users.noreply.github.com>
2019-12-31 09:09:19 +0200

    update

--- m1_run.py
+++ m1_run.py
@@ -11,7 +11,7 @@ python m1_run.py [run_i]
 
 """
 
-import sys, os
+import sys, os, time
 
 import numpy as np
 from scipy import linalg
@@ -21,8 +21,8 @@ from scipy import stats
 # ===========================================================================
 # conf (remember to save all confs in the result file)
 
-# file identifier string in '<file_id>_<run_id>'
-file_id = 'unfixed'
+# results save folder name in `res_1`
+folder_name = 'unfixed'
 
 # random seed for data
 seed = 11
@@ -32,9 +32,9 @@ fixed_sigma2_m = False
 # fixed model sigma2_m value
 sigma2_m = 1.0
 # epsilon sigma2_d
-sigma2_d = 10.0**(2*np.arange(-1, 2))
+sigma2_d = 10.0**(2*np.arange(-1, 3))
 # number of obs in one trial
-n_obs = [16, 32, 64, 128]
+n_obs = [16, 32, 64, 128, 256, 512]
 # percentage of outliers
 prc_out = [0.01, 0.05, 0.1]
 # outlier loc deviation
@@ -42,13 +42,13 @@ outlier_dev = 10.0
 # suffle observations
 suffle_obs = False
 # number of trials
-n_trial = 13
+n_trial = 2000
 # dimensionality of true beta
 n_dim = 3
 # first covariate as intercept
 intercept = True
 # last covariate effect not used in model A
-beta_t = [0.1, 1.0, 10.0]
+beta_t = [0.0, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
 # other covariates' effects
 beta_other = 1.0
 # intercept coef (if applied)
@@ -56,13 +56,6 @@ beta_intercept = 0.0
 
 
 # ===========================================================================
-# parse cmd input for run id
-if len(sys.argv) > 1:
-    # get run_i
-    run_i = int(sys.argv[1])
-else:
-    run_i = 0
-run_i_str = str(run_i).zfill(4)
 
 # set grid
 n_obs_s, beta_t_s, prc_out_s, sigma2_d_s = np.meshgrid(
@@ -77,14 +70,30 @@ prc_out_s = prc_out_s.ravel()
 sigma2_d_s = sigma2_d_s.ravel()
 n_runs = len(n_obs_s)
 
+# parse cmd input for run id
+if len(sys.argv) > 1:
+    # get run_i
+    run_i = int(sys.argv[1])
+else:
+    run_i = 0
+run_i_str = str(run_i).zfill(4)
+
 # set params for this run
 n_obs_i = n_obs_s[run_i]
 beta_t_i = beta_t_s[run_i]
 prc_out_i = prc_out_s[run_i]
 sigma2_d_i = sigma2_d_s[run_i]
 
+print('Run {}/{}'.format(run_i, n_runs))
+print(
+    'n_obs_i: {} / {}'.format(n_obs_i, n_obs),
+    'beta_t_i: {} / {}'.format(beta_t_i, beta_t),
+    'prc_out_i: {} / {}'.format(prc_out_i, prc_out),
+    'sigma2_d_i: {} / {}'.format(sigma2_d_i, sigma2_d),
+    sep='\n'
+)
 
-# ===========================================================================
+# beta vector
 beta = np.array([beta_other]*(n_dim-1)+[beta_t_i])
 if intercept:
     beta[0] = beta_intercept
@@ -142,9 +151,23 @@ def calc_loo_ti(ys, X_mat):
     mu_preds = np.empty((n_trial, n_obs_i))
     sigma2_preds = np.empty((n_trial, n_obs_i))
     # for each trial
+    inner_start_time = time.time()
     for t in range(n_trial):
-        if t % (n_trial//10) == 0:
-            print('{}/{}'.format(t, n_trial), flush=True)
+        if t % (n_trial//10) == 0 and t != 0:
+            elapsed_time = time.time() - inner_start_time
+            etr = (n_trial - t)*(elapsed_time/t)
+            etr_unit = 's'
+            if etr >= 60:
+                etr /= 60
+                etr_unit = 'min'
+                if etr >= 60:
+                    etr /= 60
+                    etr_unit = 'h'
+            etr = int(np.ceil(etr))
+            print(
+                '{}/{}, etr: {} {}'.format(t, n_trial, etr, etr_unit),
+                flush=True
+            )
         # for each data point
         for i in range(n_obs_i):
             x_i = X_mat[i]
@@ -179,6 +202,8 @@ def calc_loo_ti(ys, X_mat):
 
     return loo_ti
 
+outer_start_time = time.time()
+
 # model A
 print('model A')
 loo_ti_A = calc_loo_ti(ys, X_mat[:,:-1])
@@ -186,10 +211,24 @@ loo_ti_A = calc_loo_ti(ys, X_mat[:,:-1])
 print('model B')
 loo_ti_B = calc_loo_ti(ys, X_mat)
 
+time_per_1000 = (time.time() - outer_start_time) * 1000 / n_trial
+time_per_1000_unit = 's'
+if time_per_1000 >= 60:
+    time_per_1000 /= 60
+    time_per_1000_unit = 'min'
+    if time_per_1000 >= 60:
+        time_per_1000 /= 60
+        time_per_1000_unit = 'h'
+time_per_1000 = int(np.ceil(time_per_1000))
+print(
+    'time per 1000 trials: {} {}'
+    .format(time_per_1000, time_per_1000_unit)
+)
+
 # save
-os.makedirs('res_1', exist_ok=True)
+os.makedirs('res_1/{}'.format(folder_name), exist_ok=True)
 np.savez_compressed(
-    'res_1/{}_{}.npz'.format(file_id, run_i_str),
+    'res_1/{}/{}.npz'.format(folder_name, run_i_str),
     loo_ti_A=loo_ti_A,
     loo_ti_B=loo_ti_B,
     seed=seed,
