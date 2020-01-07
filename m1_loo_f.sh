58082b19ecc397e49d61c0ac4e36a1749cbee2e3
T. Sivula <tsivula@users.noreply.github.com>
2020-01-07 11:51:39 +0200

    update

---
+++ m1_loo_f.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=2
+#SBATCH --time=0-01:00:00 --mem-per-cpu=5000
+#SBATCH -o log/loo_fixed_%a.txt
+#SBATCH --array=0-25
+
+#module load anaconda3
+
+python m1_run_loo.py $SLURM_ARRAY_TASK_ID fixed
--- m1_loo_u.sh
+++ m1_loo_u.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=2
 #SBATCH --time=0-01:00:00 --mem-per-cpu=5000
-#SBATCH -o log/%a.txt
-#SBATCH --array=0-41
+#SBATCH -o log/loo_unfixed_%a.txt
+#SBATCH --array=0-25
 
 #module load anaconda3
 
-python m1_run.py $SLURM_ARRAY_TASK_ID
+python m1_run_loo.py $SLURM_ARRAY_TASK_ID unfixed
