60f5cb8dcbb82202cbbfd4d57279295b90e89e74
T. Sivula <tsivula@users.noreply.github.com>
2020-01-12 08:08:11 +0200

    update

--- m1_analyse.py
+++ m1_analyse.py
@@ -4,13 +4,13 @@ First run results with m1_run.py
 
 """
 
-import sys, os
-
 import numpy as np
 from scipy import linalg
 from scipy import stats
 
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 from m1_problem import *
 
@@ -33,10 +33,22 @@ else:
 # ============================================================================
 # As a function of ...
 
-# run_i_s = n_obs_idxs
+run_i_s = n_obs_idxs
+var_name = 'n_obs'
+var_vals = n_obs_s
+
 # run_i_s = sigma2_d_idxs
+# var_name = 'sigma2_d'
+# var_vals = sigma2_d_s
+
 # run_i_s = beta_t_idxs
-run_i_s = prc_out_idxs
+# var_name = 'beta_t'
+# var_vals = beta_t_s
+
+# run_i_s = prc_out_idxs
+# var_name = 'prc_out'
+# var_vals = prc_out_s
+
 
 
 n_probls = len(run_i_s)
@@ -60,27 +72,75 @@ for probl_i in range(n_probls):
     # close file
     res_file.close()
 
-# calc some true targets
-sums_s = np.zeros((n_probls, n_trial))
-mean_s = np.zeros((n_probls))
-var_s = np.zeros((n_probls))
-for probl_i in range(n_probls):
-    sums_s[probl_i] = np.sum(res_A[probl_i] - res_B[probl_i], axis=-1)
-    mean_s[probl_i] = np.mean(sums_s[probl_i])
-    var_s[probl_i] = np.var(sums_s[probl_i], ddof=1)
-coef_var = np.sqrt(var_s)/mean_s
-
-# calc corr
+# calc some normally obtainable values
+loo_s = np.zeros((n_probls, n_trial))
+var_estim_s = np.zeros((n_probls, n_trial))
 cor_s = np.zeros((n_probls, n_trial))
 for probl_i in range(n_probls):
+    n_cur = res_A[probl_i].shape[-1]
+    loo_s[probl_i] = np.sum(res_A[probl_i]-res_B[probl_i], axis=-1)
+    var_estim_s[probl_i] = n_cur*np.var(
+        res_A[probl_i]-res_B[probl_i], ddof=1, axis=-1)
     for trial_i in range(n_trial):
         cor_s[probl_i, trial_i] = np.corrcoef(
             res_A[probl_i][trial_i], res_B[probl_i][trial_i])[0, 1]
+coef_var_s = np.sqrt(var_estim_s)/loo_s
 
-# plot
-fig, axes = plt.subplots(n_probls, sharex=True)
+# calc some test values
+true_mean_s = np.zeros((n_probls))
+true_var_s = np.zeros((n_probls))
+true_plooneg = np.zeros((n_probls))
+elpd_s = np.zeros((n_probls, n_trial))
+for probl_i in range(n_probls):
+    true_mean_s[probl_i] = np.mean(loo_s[probl_i])
+    true_var_s[probl_i] = np.var(loo_s[probl_i], ddof=1)
+    true_plooneg[probl_i] = np.mean(loo_s[probl_i]<0)
+    elpd_s[probl_i] = res_test_A[probl_i] - res_test_B[probl_i]
+true_coef_var_s = np.sqrt(true_var_s)/true_mean_s
+
+# plots
+use_sea = True
+
+# LOO and elpd
+fig, axes = plt.subplots(1, n_probls, sharey=False, figsize=(16,8))
 for ax, probl_i in zip(axes, range(n_probls)):
-    ax.hist(cor_s[probl_i], 20)
+    if use_sea:
+        sns.violinplot(
+            data=[loo_s[probl_i], elpd_s[probl_i]],
+            orient='v',
+            scale='width',
+            ax=ax
+        )
+    else:
+        ax.hist(elpd_s[probl_i], 20, color='C1')
+        ax.hist(loo_s[probl_i], 20, color='C0')
+    ax.set_title(var_vals[probl_i])
+fig.suptitle('LOO and test elpd (y={})'.format(var_name))
+fig.tight_layout()
 
-fig = plt.figure()
-plt.plot(np.mean(cor_s, axis=-1), coef_var, '.')
+# coef of vars
+fig, axes = plt.subplots(1, n_probls, sharey=False, figsize=(16,8))
+for ax, probl_i in zip(axes, range(n_probls)):
+    if use_sea:
+        sns.violinplot(coef_var_s[probl_i], orient='v', ax=ax)
+    else:
+        ax.hist(coef_var_s[probl_i], 20)
+    ax.axhline(true_coef_var_s[probl_i], color='r')
+    ax.set_title(var_vals[probl_i])
+fig.suptitle(var_name)
+fig.tight_layout()
+
+# cors
+fig, axes = plt.subplots(1, n_probls, sharey=False, figsize=(16,8))
+for ax, probl_i in zip(axes, range(n_probls)):
+    if use_sea:
+        sns.violinplot(cor_s[probl_i], orient='v', ax=ax)
+    else:
+        ax.hist(cor_s[probl_i], 20)
+    ax.set_title(var_vals[probl_i])
+fig.suptitle(var_name)
+fig.tight_layout()
+
+fig, axes = plt.subplots(2, 1, sharex=True)
+axes[0].plot(np.mean(cor_s, axis=-1), true_coef_var_s, '.')
+axes[1].plot(np.mean(cor_s, axis=-1), true_plooneg, '.')
