4a385eb57ba985aba7cc6c5b9b3de6524c05dc3a
T. Sivula <tsivula@users.noreply.github.com>
2020-01-16 08:43:53 +0200

    update

--- m1_l_f.sh
+++ m1_l_f.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=2
-#SBATCH --time=0-01:00:00 --mem-per-cpu=5000
+#SBATCH --time=0-03:00:00 --mem-per-cpu=5000
 #SBATCH -o log/loo_fixed_%a.txt
-#SBATCH --array=0-25
+#SBATCH --array=0-575
 
 #module load anaconda3
 
--- m1_mb_u.sh
+++ m1_mb_u.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --time=2-00:00:00 --mem-per-cpu=5000
 #SBATCH -o log/mboot_unfixed_%a.txt
-#SBATCH --array=0-25
+#SBATCH --array=0-575
 
 #module load anaconda3
 
