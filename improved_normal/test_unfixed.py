ca747142667c64f6becd80d0ea18e314d1223f12
tsivula <tsivula@users.noreply.github.com>
2020-08-12 15:57:24 +0300

    u

--- improved_normal/test_unfixed.py
+++ improved_normal/test_unfixed.py
@@ -1,6 +1,8 @@
 
 import itertools
 
+from tqdm import tqdm
+
 import numpy as np
 from scipy import linalg, stats
 from scipy.special import comb
@@ -13,6 +15,9 @@ import matplotlib.pyplot as plt
 
 seed = 57346234
 n_trial = 2000
+
+n_loo_mc = 1000
+n_impr_mc = 1000
 
 # data params
 n_obs = 16
@@ -40,12 +45,15 @@ datadist = stats.norm(loc=0, scale=1.2)
 # exkurt = 0.82
 
 
-# model params
-# sigma2_m = 1.2**2
-sigma2_p = 2.0**2
+# priors
+# conjugate prior BDA 3rd ed pp. 67-68
+sigma2_0 = 2.0**2
+k_0 = 0.1
+v_0 = 10
 
 
 # ==============================================================================
+
 
 rng = np.random.RandomState(seed)
 
@@ -62,16 +70,47 @@ print(f'mu = {mu:.2},\nsigma2 = {sigma2:.2},\nskew = {skew:.2},\nexkurt = {exkur
 
 
 def get_loo_ti(data_ti):
+
+    # noninformative prior
+    # out = np.zeros((n_trial, n_obs))
+    # for i in range(n_obs):
+    #     ytilde_t = data_ti[:, i]
+    #     y_t = np.delete(data_ti, i, axis=-1)
+    #     n_obs_cur = y_t.shape[-1]
+    #     ybar_t = np.mean(y_t, axis=-1)
+    #     s2_t = np.var(y_t, ddof=1, axis=-1)
+    #     scale_t = s2_t*np.sqrt(1+1/n_obs_cur)
+    #     df = n_obs_cur - 1
+    #     out[:, i] = stats.t.logpdf(ytilde_t, df, loc=ybar_t, scale=scale_t)
+
+    # conjugate prior BDA 3rd ed pp. 67-68
     out = np.zeros((n_trial, n_obs))
-    for i in range(n_obs):
+    for i in tqdm(range(n_obs)):
         ytilde_t = data_ti[:, i]
         y_t = np.delete(data_ti, i, axis=-1)
         n_obs_cur = y_t.shape[-1]
         ybar_t = np.mean(y_t, axis=-1)
         s2_t = np.var(y_t, ddof=1, axis=-1)
-        scale_t = s2_t*np.sqrt(1+1/n_obs_cur)
-        df = n_obs_cur - 1
-        out[:, i] = stats.t.logpdf(ytilde_t, df, loc=ybar_t, scale=scale_t)
+        mu_n = (n_obs_cur/(k_0 + n_obs_cur))*ybar_t
+        k_n = k_0 + n_obs_cur
+        v_n = v_0 + n_obs_cur
+        v_n_s2_n = (
+            v_0*sigma2_0
+            + (n_obs_cur-1)*s2_t
+            + n_obs_cur*k_0/(n_obs_cur+k_0)*ybar_t**2
+        )
+        alpha = 0.5*v_n
+        # beta = 0.5*s2_n*v_n
+        beta = 0.5*v_n_s2_n
+        s2_samp = stats.invgamma.rvs(
+            alpha, scale=beta, size=(n_loo_mc, n_trial), random_state=rng)
+        temp = s2_samp/k_n
+        np.sqrt(temp, out=temp)
+        mu_samp = stats.norm.rvs(
+            loc=mu_n, scale=temp, random_state=rng)
+        np.sqrt(s2_samp, out=temp)
+        out[:, i] = np.log(np.mean(
+            stats.norm.pdf(ytilde_t, loc=mu_samp, scale=temp), axis=0))
     return out
 
 
@@ -127,25 +166,46 @@ def get_moment_estims(data_ti):
         + (-12 + 8*n-4*n**2)*n*a3_a1
         + (3*n-2*n**2+n**3)*a4
     ) / ((n-3)*(n-2)*(n-1))
-    # s2_hat
-    s2_hat = np.var(data_ti, ddof=1, axis=-1)
-    return s2_hat, mup2_s2_hat, s4_hat, mu_mu3_hat, mu4_hat
+    return mup2_s2_hat, s4_hat, mu_mu3_hat, mu4_hat
 
 
-def improved_estim(s2_hat, mup2_s2_hat, s4_hat, mu_mu3_hat, mu4_hat):
+def improved_estim(data_ti):
     n = n_obs
 
-    a = (
-        -0.5*(s2_hat + (n_obs-1)*sigma2_p)
-        / (s2_hat*(s2_hat+n_obs*sigma2_p))
+    # full data marginal posterior for sigma_m
+    ybar_t = np.mean(data_ti, axis=-1)
+    s2_t = np.var(data_ti, ddof=1, axis=-1)
+    # mu_n = (n/(k_0 + n))*ybar_t
+    # k_n = k_0 + n
+    v_n = v_0 + n
+    v_n_s2_n = (
+        v_0*sigma2_0
+        + (n-1)*s2_t
+        + n*k_0/(n+k_0)*ybar_t**2
     )
-    b = (n_obs-1)*sigma2_p/(s2_hat*(s2_hat+n_obs*sigma2_p))
+    alpha = 0.5*v_n
+    # beta = 0.5*s2_n*v_n
+    beta = 0.5*v_n_s2_n
+    s2_m_samp = stats.invgamma.rvs(
+        alpha, scale=beta, size=(n_impr_mc, n_trial), random_state=rng)
+
+    sigma2_p_samp = s2_m_samp / k_0
+
+    # continue as with fixed var
+    mup2_s2_hat, s4_hat, mu_mu3_hat, mu4_hat = get_moment_estims(data_ti)
+
+    a = (
+        -0.5*(s2_m_samp + (n-1)*sigma2_p_samp)
+        / (s2_m_samp*(s2_m_samp+n*sigma2_p_samp))
+    )
+    b = (n-1)*sigma2_p_samp/(s2_m_samp*(s2_m_samp+n*sigma2_p_samp))
     c = (
-        -0.5*(n_obs-1)**2*sigma2_p**2
-        / (s2_hat*(s2_hat + (n_obs-1)*sigma2_p)*(s2_hat+n_obs*sigma2_p))
+        -0.5*(n-1)**2*sigma2_p_samp**2
+        / (s2_m_samp*(s2_m_samp + (n-1)*sigma2_p_samp)
+            *(s2_m_samp+n*sigma2_p_samp))
     )
 
-    var_hat = (
+    var_hat_samp = (
         mup2_s2_hat * 4*n*(a+b+c)**2
         + s4_hat * (
                 -n*a**2
@@ -157,13 +217,14 @@ def improved_estim(s2_hat, mup2_s2_hat, s4_hat, mu_mu3_hat, mu4_hat):
         + mu_mu3_hat * 4*n*(a+b+c)*(a*(n-1)+c)/(n-1)
         + mu4_hat * (n*a**2 + n/(n-1)**2*c**2 + 2*n/(n-1)*a*c)
     )
-    return var_hat
+    return np.mean(var_hat_samp, axis=0)
 
 
 # ==============================================================================
 # simulate
 
 data_ti = datadist.rvs(size=(n_trial, n_obs), random_state=rng)
+print('getting loos ...')
 loo_ti = get_loo_ti(data_ti)
 loo_t = np.sum(loo_ti, axis=-1)
 
@@ -173,12 +234,8 @@ target_var = np.var(loo_t, ddof=1)
 # naive
 var_hat_naiv_t = n_obs*np.var(loo_ti, ddof=1, axis=-1)
 
-# moment estims
-s2_hat, mup2_s2_hat, s4_hat, mu_mu3_hat, mu4_hat = get_moment_estims(data_ti)
-
 # get improved estims
-var_hat_impr_t = improved_estim(
-    s2_hat, mup2_s2_hat, s4_hat, mu_mu3_hat, mu4_hat)
+var_hat_impr_t = improved_estim(data_ti)
 
 
 # ============================================================================
@@ -206,11 +263,10 @@ datas_point = [
 names = ['naive', 'improved']
 
 fig, axes = plt.subplots(2, 1, sharex=True, figsize=(4, 3))
-for ax, data, data_point, name in zip(axes, datas, datas_point, names):
+for ax, data, name in zip(axes, datas, names):
     # data_filtered = data[data<uplim]
     ax.hist(data, bins=20, label='BB')
     ax.axvline(1.0, color='k', lw=0.8)
-    ax.axvline(data_point, color='C1', ls='--', label='analytic')
 
     # ax.set_xticks([0, 1, 2, 3])
 
@@ -222,11 +278,11 @@ for ax, data, data_point, name in zip(axes, datas, datas_point, names):
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
 
-    # ax.set_title(name)
+    ax.set_ylabel(name)
 
-# axes[1].legend(
-#     loc='lower left',
-#     fancybox=False, shadow=False, framealpha=1.0)
+
+
+fig.suptitle(f'n={n_obs}')
 
 axes[-1].set_xlabel(
     r'$\sqrt{\left.\mathrm{E}\left[\widehat{\sigma^2_\mathrm{LOO}}\right]'
