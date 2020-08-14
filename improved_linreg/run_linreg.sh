663b756698ecdfca7ca34024eb778579c3ad9bae
tsivula <tsivula@users.noreply.github.com>
2020-08-14 13:59:15 +0300

    Move experimental linreg to separate branch

--- improved_linreg/run_linreg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --cpus-per-task=4
-#SBATCH --time=3-00:00:00 --mem-per-cpu=5000
-#SBATCH -o log/linreg_%a.txt
-#SBATCH --array=0-69
-
-#module load anaconda3
-
-python run_linreg.py $SLURM_ARRAY_TASK_ID
