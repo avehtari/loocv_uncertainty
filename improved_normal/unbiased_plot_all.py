91770f99edda9eb9067497f5fa1762799a50698c
tsivula <tsivula@users.noreply.github.com>
2020-08-22 20:25:49 +0300

    Update unbiased estim experiment fig

--- improved_normal/unbiased_plot_all.py
+++ improved_normal/unbiased_plot_all.py
@@ -26,12 +26,6 @@ sigma2_p = 2.0**2
 
 # ==============================================================================
 # hard-coded results from `unbiased.py`
-problem_names = [
-    'well-fitting data',
-    'under-dispersed\ndata',
-    'under-dispersed, skewed,\nand heavy-tailed data'
-]
-estim_names = ['naive', 'unbiased']
 bb_mean_pe = [
     [0.9663997725, 1.000294244],
     [1.15876797, 1.000242305],
@@ -78,6 +72,83 @@ plt.rcParams["mathtext.fontset"] = "dejavuserif"
 # plot
 # ============================================================================
 
+# # -------------------------------------
+# # y-axis
+#
+# problem_names = [
+#     'Well-matching data',
+#     'Under-dispersed data',
+#     'Under-dispersed, skewed,\nand heavy-tailed data'
+# ]
+# estim_names = ['naive', 'unbiased']
+#
+# x_sect_lims = np.linspace(0, 1, len(estim_names)+1)
+# x_cents = np.linspace(0, 1, 2*len(estim_names)+1)[1::2]
+# x_space = 0.8  # between [0, 1]
+# x_delta = (x_sect_lims[1]-x_sect_lims[0])*x_space/3
+# x_grid = (
+#     x_sect_lims[:-1] + (x_sect_lims[1]-x_sect_lims[0])*(1-x_space)
+#     + x_delta*np.arange(1, 3)[:, None]
+# )
+# x_analytic = x_grid[0]
+# x_bb = x_grid[1]
+#
+# err_pe = (
+#     (np.array(bb_mean_pe)[: ,:, None] - np.array(bb_ci_99_pe)) *
+#     np.array([[[1, -1]]])
+# )
+#
+#
+# fig, axes = plt.subplots(1, len(problem_names), figsize=(6, 3))
+# # fig, axes = plt.subplots(1, len(problem_names), sharey=True, figsize=(6, 5))
+# for ax, probl_name, bb_median_p, err_p, analytic_p in zip(
+#         axes, problem_names, bb_median_pe, err_pe, analytic_pe):
+#
+#     ax.axhline(1.0, lw=1.0, color='C2', label='target')
+#
+#     # analytic
+#     ax.plot(
+#         x_analytic,
+#         analytic_p,
+#         '.',
+#         markersize=11,
+#         label='analytic',
+#         color='C0',
+#     )
+#
+#     # bb
+#     ax.errorbar(
+#         x_bb,
+#         bb_median_p,
+#         yerr=err_p,
+#         fmt='.',
+#         markersize=11,
+#         label='BB',
+#         color='C1',
+#     )
+#
+#     ax.set_xlim([0, 1])
+#     ax.set_xticks(x_cents)
+#     ax.set_xticklabels(estim_names)
+#
+#     ax.spines['top'].set_visible(False)
+#     ax.spines['right'].set_visible(False)
+#
+#     ax.set_title(probl_name, fontsize=11)
+#
+# axes[-1].legend(fancybox=False, shadow=False, framealpha=1.0)
+#
+# fig.tight_layout()
+
+# -------------------------------------
+# x-axis
+
+problem_names = [
+    'Well-matching data',
+    'Under-dispersed data',
+    'Under-dispersed, skewed, and heavy-tailed data'
+]
+estim_names = ['naive', 'unbiased']
 
 x_sect_lims = np.linspace(0, 1, len(estim_names)+1)
 x_cents = np.linspace(0, 1, 2*len(estim_names)+1)[1::2]
@@ -87,6 +158,7 @@ x_grid = (
     x_sect_lims[:-1] + (x_sect_lims[1]-x_sect_lims[0])*(1-x_space)
     + x_delta*np.arange(1, 3)[:, None]
 )
+x_grid = 1 - x_grid
 x_analytic = x_grid[0]
 x_bb = x_grid[1]
 
@@ -95,42 +167,60 @@ err_pe = (
     np.array([[[1, -1]]])
 )
 
-fig, axes = plt.subplots(1, len(problem_names), sharey=True)
+# fig, axes = plt.subplots(len(problem_names), 1, figsize=(6, 5))
+fig, axes = plt.subplots(len(problem_names), 1, sharex=True, figsize=(7, 4.5))
 for ax, probl_name, bb_median_p, err_p, analytic_p in zip(
         axes, problem_names, bb_median_pe, err_pe, analytic_pe):
 
-    ax.axhline(1.0, lw=1.0, color='C2', label='target')
+    l1 = ax.axvline(1.0, lw=1.0, color='C2', label='target')
 
     # analytic
-    ax.plot(
-        x_analytic,
+    l2, = ax.plot(
         analytic_p,
+        x_analytic,
         '.',
-        markersize=14,
+        markersize=11,
         label='analytic',
         color='C0',
     )
 
     # bb
-    ax.errorbar(
-        x_bb,
+    l3 = ax.errorbar(
         bb_median_p,
-        yerr=err_p,
+        x_bb,
+        xerr=err_p,
         fmt='.',
-        markersize=14,
+        markersize=11,
         label='BB',
         color='C1',
     )
 
-    ax.set_xlim([0, 1])
-    ax.set_xticks(x_cents)
-    ax.set_xticklabels(estim_names)
+    ax.set_ylim([0, 1])
+    ax.set_yticks(x_cents)
+    ax.set_yticklabels(np.array(estim_names)[::-1])
 
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
 
+    # ax.set_ylabel(probl_name, fontsize=11, rotation=0, ha='right', va='center')
     ax.set_title(probl_name, fontsize=11)
 
-axes[-1].legend(fancybox=False, shadow=False, framealpha=1.0)
+axes[-1].set_xlabel(
+    r'$\sqrt{\left.\mathrm{E}\left[\widehat{\sigma^2_\mathrm{LOO}}\right]'
+    r'\;\right/\;'
+    r'\sigma^2_\mathrm{LOO}}$',
+)
+
+axes[-1].legend(
+    # [l1, l3, l2],
+    # ['target', 'BB', 'analytic'],
+    ncol=3,
+    loc='upper center',
+    bbox_to_anchor=(0.5, -0.9),
+    fancybox=False,
+    shadow=False,
+    framealpha=1.0,
+)
 
 fig.tight_layout()
+fig.subplots_adjust(bottom=0.24)
